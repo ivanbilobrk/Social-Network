401eac85a068d80be6c772a6f4dd877657cca09f
LovroK <lovro.kovacic7@gmail.com>
2022-11-16 16:09:41 +0100

    removed app.css import

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -1,40 +1,35 @@
-import "./App.css";
-import { SignupPage } from "./pages/SignupPage";
-import { Route, Routes } from "react-router-dom";
+import { SignupPage } from './pages/SignupPage';
+import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
-import { LoginPage } from "./pages/LoginPage";
-import Test from "./pages/Test";
-import RequireAuth from "./components/auth/RequireAuth";
-import Unauthorized from "./pages/Unauthorized";
-import BlockAuth from "./components/auth/BlockAuth";
+import { LoginPage } from './pages/LoginPage';
+import Test from './pages/Test';
+import RequireAuth from './components/auth/RequireAuth';
+import Unauthorized from './pages/Unauthorized';
+import BlockAuth from './components/auth/BlockAuth';
 import { Grid } from '@mui/material';
 import React from 'react';
 import Home from './pages/Home';
-import HomePage from "./pages/HomePage";
-
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-        <>
-          <Routes>
-
-          <Route path ="/" element={<Layout/>}>
-
-            <Route element={<RequireAuth allowedRoles={["user"]}/>}>
-                <Route path="/test" element={<Test/>}/>
-                <Route path="/home" element={<HomePage/>}></Route>
-            </Route>
-            <Route path="unauthorized" element={<Unauthorized />} />
-            
-            <Route element={<BlockAuth/>}>
-              <Route path="signup" element = {<SignupPage/>}></Route>
-              <Route path="login" element = {<LoginPage/>}></Route>
-            </Route>
-            
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route element={<RequireAuth allowedRoles={['user']} />}>
+            <Route path="/test" element={<Test />} />
+            <Route path="/home" element={<HomePage />}></Route>
           </Route>
+          <Route path="unauthorized" element={<Unauthorized />} />
 
-        </Routes>
-        </>
-  )}
+          <Route element={<BlockAuth />}>
+            <Route path="signup" element={<SignupPage />}></Route>
+            <Route path="login" element={<LoginPage />}></Route>
+          </Route>
+        </Route>
+      </Routes>
+    </>
+  );
+}
 
 export default App;
