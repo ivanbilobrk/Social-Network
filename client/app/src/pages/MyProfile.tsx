3801d2829d4fce9c6a6f6a43877d14e127f9ff27
MARA\marik <marikompar@gmail.com>
2022-12-12 16:01:32 +0100

    kasnije cu nastavit

--- client/app/src/pages/MyProfile.tsx
+++ client/app/src/pages/MyProfile.tsx
@@ -1,11 +1,24 @@
 import {Grid} from '@mui/material';
 import Profile from '../components/Profile';
+import Header from '../components/Header';
 
-function MyProfile(props : any) {
+const username = "sammy1";
+const fullname = "Samantha Jones";
+const followers = 12345;
+const following = 10;
+const posts = 10;
+
+const MyProfile = () => {
     return (
-        
-        <Profile username = {props.username} fullname = {props.fullname} followers = {props.followers} following = {props.following}  posts = {props.posts}/>
-    )
-}
+        <>
+        <Header></Header>
+        <Grid container direction = "column" alignItems = "center" justifyContent = "center">
+            <Grid item width = "40%">
+                <Profile username = {username} fullname = {fullname} followers = {followers} following = {following}  posts = {posts}/>
+            </Grid>
+        </Grid>
+        </>
+    );
+};
 
 export default MyProfile;
