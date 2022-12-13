9d554496553a121105d0e529cac0da754488b890
LovroK <lovro.kovacic7@gmail.com>
2022-12-13 13:11:37 +0100

    started working on comment popup

---
+++ client/app/src/components/Comment.tsx
@@ -0,0 +1,30 @@
+import { Avatar, Box, Card, CardActions, CardContent, CardHeader, Grid, IconButton, Typography } from '@mui/material';
+import { date } from 'yup';
+
+function Comment(props: any) {
+  <Card
+    variant="elevation"
+    style={{ backgroundColor: 'silver', width: '100%', aspectRatio: 1.2, maxHeight: '30rem' }}
+    sx={{ my: '1rem' }}
+  >
+    <CardHeader avatar={<Avatar alt="Remy Sharp" src="https://source.unsplash.com/random" />} title={props.username} />
+    <Box
+      component="img"
+      sx={{ width: '100%', height: '17rem', objectFit: 'cover' }}
+      alt="The house from the offer."
+      src="https://source.unsplash.com/random"
+    />
+    <Box />
+
+    <CardActions>
+      <Grid container direction={'row'} justifyContent="flex-start" alignItems={'center'}></Grid>
+    </CardActions>
+    <CardContent sx={{ pt: '0rem' }}>
+      <Typography variant="body1" component="div">
+        {props.description}
+      </Typography>
+    </CardContent>
+  </Card>;
+}
+
+export default Comment;
